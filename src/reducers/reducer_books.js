29cc2b12b25c19d2ff98e7d300b7d81c9abf62d1
Nevena Jovanovic <nevena@3ap.ch>
2018-05-31 14:34:06 +0200

    Add files via upload

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+ export default function() {
+    return [
+        {title:  'Javascript: The Good Parts', pages: 101},
+        {title:  'The Dark Tower', pages: 555},
+        {title:  'Harry Potter', pages: 34},
+        {title:  'Ruby', pages: 74}
+    ]
+ }
